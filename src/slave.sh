e549cc6a54ba0ea02bf69fb6dadf6a95f357b900
Justin Menga <justin.menga@gmail.com>
2017-03-18 19:45:18 +1300

    Change default label to docker

--- src/slave.sh
+++ src/slave.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
+# Variable defaults
+url=${JENKINS_URL:-http://jenkins:8080/}
+username=${JENKINS_USERNAME:-admin}
+labels=${JENKINS_SLAVE_LABELS:-docker}
+
 # Start slave
-exec java -jar slave.jar -master ${JENKINS_URL:-http://jenkins:8080/} -username ${JENKINS_USERNAME} -passwordEnvVariable JENKINS_PASSWORD -executors 1 -labels ${JENKINS_SLAVE_LABELS:-DOCKER}
+exec java -jar slave.jar -master $url -username $username -passwordEnvVariable JENKINS_PASSWORD -executors 1 -labels ${labels}
